591b93ae387884b4d80c904f68eda19fa1573700
gabbismart <48329130+gabbismart@users.noreply.github.com>
2021-09-23 22:56:38 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import logo from './logo.svg';
+import './App.css';
+import from 
+const App = () =>
+<Profile fullName="Sianu Gabriel Bright" bio="Born in Lagos, Schooled in Lagos and working in Lagos" profession="Information Technologist">
+  <p>handleName</p>
+</Profile>
+
+export default App;
---
+++ src/Profile/profilecomponent.js
@@ -0,0 +1,7 @@
+const Profile = ({fullName="Sianu Gabriel Bright", bio="Born in Lagos, Schooled in Lagos and working in Lagos", profession="Information Technologist", handleName="fullName"}) => <div>
+    <p>{fullName}</p>
+    <p>{bio}</p>
+    <p>{profession}</p>
+</div>
+
+export default profile
